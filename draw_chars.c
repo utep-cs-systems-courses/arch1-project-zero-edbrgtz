0aa46b422594bcb5b32e6488e6b2df13e0d1fdcd
Eduardo Briones <90985690+edbrgtz@users.noreply.github.com>
2022-09-16 10:10:00 -0600

    Fixed bugs

--- draw_chars.c
+++ draw_chars.c
@@ -17,9 +17,9 @@ void print_char_11x16(char c)
 void print_char_8x12(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 11; row++) {
+  for (char row = 0; row < 8; row++) {
     unsigned char rowBits = font_8x12[c][row];
-    for (char col = 0; col < 16; col++) {
+    for (char col = 0; col < 12; col++) {
       unsigned char colMask = 1 << (15-col); /* mask to select bit associated with bit */
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
